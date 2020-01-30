e18da41c7b719d17abea33cbfe4002c2fd06546e
gabriella <gabriella.mosca@gmail.com>
2020-01-30 18:54:46 +0100

    Added some meshes and real template model

---
+++ realTemplate/MorphoDynamX.py
@@ -0,0 +1,4 @@
+######## New MorphoDynamX session v2.0 r2-674-ge6183014: 2020-01-30 18:32:33
+Process.Tools__System__Save_View('/home/gabriella/IMAGO_project/git_repoMassSpring2D/MassSpring_2DovuleGrowthModel/withMegaSpore/comparisonImageSettingFinalPaperMMC_notDivromT110.mdxv')
+Process.Mesh__System__Load('/home/gabriella/IMAGO_project/git_repoMassSpring2D/MassSpring_2DovuleGrowthModel/realTemplate/mesh-2018_2404_275aSegmentedMeshMergingSplittingNonGrowingPoligonizedAssignedPressurized_latest.mdxm', 'no', 'no', '0')
+Process.Tools__System__Load_View('/home/gabriella/IMAGO_project/git_repoMassSpring2D/MassSpring_2DovuleGrowthModel/case3/CellDisk.mdxv')
